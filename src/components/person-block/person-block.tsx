2ea9a43cafeaa0a93d318ffca8b3a14caba790e5
SiarheiHa <belposchta@gmail.com>
2023-03-12 22:07:00 +0300

    fix: fix singout

--- src/components/person-block/person-block.tsx
+++ src/components/person-block/person-block.tsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import classNames from 'classnames';
 
 import { ReactComponent as Avatar } from '../../assets/images/avatar.svg';
+import { resetUserState, useAppDispatch } from '../../store';
 
 import styles from './person-block.module.scss';
 
 const PersonBlock = ({ className }: { className?: string }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
   const classes = classNames(className, styles.wrapper);
+  const menuClasses = classNames(styles.menu, menuVisible && styles.show);
+  const dispatch = useAppDispatch();
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const singout = () => {
+    localStorage.clear();
+    dispatch(resetUserState());
+  };
 
   return (
     <div className={classes}>
       <span className={styles.greeting}>Привет, Иван!</span>
       <div className={styles.image_wrapper}>
-        <Avatar className={styles.avatar} />
+        <Avatar className={styles.avatar} onClick={toggleMenu} />
+      </div>
+      <div className={menuClasses}>
+        <p className={styles.item}>Профиль</p>
+        <button type='button' onClick={singout} className={styles.button}>
+          <p className={styles.item}>Выход</p>
+        </button>
       </div>
     </div>
   );
